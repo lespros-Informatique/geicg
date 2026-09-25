2fe206200754c6b6064f23fc68bba8377f631176
Aboutreyke9 <absanogo9@gmail.com>
2026-09-25 15:31:17 +0000

    bien

--- views/templates/pdf/fiche_inscription.php
+++ views/templates/pdf/fiche_inscription.php
@@ -398,7 +398,7 @@ if ($montant_operation != 105000) {
           <img src="<?= $logoSrc ?>" alt="Logo EICG" class="header-logo-img">
         </td>
         <td class="header-text-cell">
-          <h1 class="header-title" style="color: #800000; font-weight: bold;">GROUPE ECOLE INTERNATIONALE DE COMMERCE ET DE GESTION</h1>
+          <h1 class="header-title" style="color: #700303ff; font-weight: bold;">GROUPE ECOLE INTERNATIONALE DE COMMERCE ET DE GESTION</h1>
           <div class="header-subtitle-1">Agréé par l'Etat et le FDFP</div>
           <div class="header-subtitle-2">Contacts : 27 31 62 40 57 / 07 79 37 37 38 / 05 04 59 39 99</div>
         </td>
